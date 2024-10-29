944bda8d0f391245a625773ebde5437510c7055e
Robert Guz <guzleon@gmx.de>
2024-10-29 14:25:37 +0100

    added outline for kuroda normalform

--- lba.js
+++ lba.js
@@ -56,19 +56,68 @@ function convert_to_kuroda(grammar){
 
     for(const i in grammar.productions){
         if(i.left.length + 2 <=  i.right.length){
+            //new Variables D2..Dn-1
 
+            //A1A2->B1D2
+            //left side = [grammar.productions[i].left[0], grammar.productions[i].left[1]]
+            //right side = [grammar.productions[i].right[0], "D2"]
+
+
+            //DiAi+1->BiDi+1
+            for(let j = 2; j < grammar.productions[i].right.length - 1; j++){
+                //left_side of production = ["D" + j.toString(), grammar.productions[i].left[j+1]]
+                //right side of production = [grammar.productions[i].right[j], "D" + (j + 1).toString()]
+            }
+
+            //Dn-1->BnBn+1
+            //n = grammar.productions[i].right.length
+            //left_side = ["D" + (n - 1).toString()]
+            //right side = [grammar.productions[i].right[n], grammar.productions[i].right[n+1]]
         }
     }
 
     for(const i in grammar.productions){
         if(i.left.length  == i.right.length + 1 && i.left.length >= 2){
+            //new Variables D2..Dn
+
+            //A1A2->B1D2
+            //left side = [grammar.productions[i].left[0], grammar.productions[i].left[1]]
+            //right side = [grammar.productions[i].right[0], "D2"]
+
+
+            //DiAi+1->BiDi+1
+            for(let j = 2; j < grammar.productions[i].right.length - 1; j++){
+                //left_side of production = ["D" + j.toString(), grammar.productions[i].left[j+1]]
+                //right side of production = [grammar.productions[i].right[j], "D" + (j + 1).toString()]
+            }
+
+            //Dn-1->BnBn+1
+            //n = grammar.productions[i].right.length
+            //left_side = ["D" + (n - 1).toString()]
+            //right side = [grammar.productions[i].right[n], grammar.productions[i].right[n+1]]
 
         }
     }
 
     for(const i in grammar.productions){
         if(i.left.length  == i.right.length && i.left.length > 2){
+             //new Variables D2..Dn-1
 
+            //A1A2->B1D2
+            //left side = [grammar.productions[i].left[0], grammar.productions[i].left[1]]
+            //right side = [grammar.productions[i].right[0], "D2"]
+
+
+            //DiAi+1->BiDi+1
+            for(let j = 2; j < grammar.productions[i].right.length - 1; j++){
+                //left_side of production = ["D" + j.toString(), grammar.productions[i].left[j+1]]
+                //right side of production = [grammar.productions[i].right[j], "D" + (j + 1).toString()]
+            }
+
+            //Dn-1An->Bn-1Bn
+            //n = grammar.productions[i].right.length
+            //left_side = ["D" + (n - 1).toString(), grammar.productions[i].left[n]]
+            //right side = [grammar.productions[i].right[n-1], grammar.productions[i].right[n]]
         }
     }   
 }
@@ -83,9 +132,13 @@ function grammar_to_lba(grammar){
     for(const i in grammar.productions){
         if(i.right.length == 1){            
             //for A->a or A->B replace current symbol with A
+            //(z,a) -> (z,A,L)
+            //(z,B) -> (z,B,L)
         } else if(i.right.length == 2){
             if(i.left.length == 2){
                 //for AB->CD, write B, change head to left, write A 
+                //(z,D) -> (z,B,L)
+                // if C (z,C) -> (z,A,L), if no C -> go back right
             }else if(i.left.length == 1){
                 //for A->BC, write A, change head to left, write x, change head to left and do step 2 until x is replaced
         
