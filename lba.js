b83d013a303fadaa6e11c3d0c6f178a823c3870f
Robert Guz <guzleon@gmx.de>
2024-11-12 01:29:17 +0100

    finished algorithm to convert grammar to kuroda normalform

--- lba.js
+++ lba.js
@@ -1,4 +1,4 @@
-class Productions{
+class Production{
     constructor(left, right){
         this.left = left
         this.right = right
@@ -21,105 +21,132 @@ class Grammar{
 function convert_to_kuroda(grammar){
     //seperate terminals
     for(let i = 0; i < grammar.terminals.length; i++){
-        old_variable = grammar.terminals[i]
+        old_symbol = grammar.terminals[i]
         new_variable = grammar.terminals[i].toUpperCase().concat(grammar.terminals[i])
 
         grammar.terminals[i] = new_variable
 
-        for(const i in grammar.productions){
-            i.right.replace(old_variable, new_variable)
+        for(let j = 0; j < grammar.productions.length; j++){
+            //replace nonterminal in left side of production
+            for(let k = 0; k < grammar.productions[j].left.length; k++){
+                if(grammar.productions[j].left[k] == old_symbol){
+                    grammar.productions[j].left[k] = new_variable
+                }
+            }
+            
+            //replace nonterminal in right side of production
+            for(let k = 0; k < grammar.productions[j].right.length; k++){
+                if(grammar.productions[j].right[k] == old_symbol){
+                    grammar.productions[j].right[k] = new_variable
+                }
+            }
+        }
+
+        grammar.productions.push(new Production([new_variable], [old_symbol]))
+    }
+
+    
+
+    for(let i = 0; i < grammar.productions.length; i++){
+        n = grammar.productions[i].right.length 
+
+        if(grammar.productions[i].left.length == 1 && n > 2){
+            //new variables C1..CN-2, replace Productions A -> B1..BN with A->B1C1..Ci->Bi+1Ci+1
+            //A->B1C1
+            grammar.productions.push(new Production(grammar.productions[i].left, [grammar.productions[i].right[0], "C0"]))
+
+            //C1->B2C2..Bn-3Cn-3
+            for(let j = 0; j < n - 3; j++){
+                grammar.productions.push(new Production(["C" + j.toString()], [grammar.productions[i].right[j+1], "C" + (j + 1).toString()]))
+            }
+
+            //Cn-2->Bn-1Bn
+            grammar.productions.push(new Production(["C" + (n - 3).toString()], [grammar.productions[i].right[n-2], grammar.productions[i].right[n-1]]))
+
+            //remove replaced production
+            grammar.productions.splice(i, 1)
+            i -= 1 
         }
     }
 
     for(let i = 0; i < grammar.productions.length; i++){
-        
-        if(grammar.productions[i].left.length == 1 && grammar.productions[i].right.length > 2){
-            //new variables C1..CN-2, replace Productions A -> B1..BN with A->B1C1..Ci->Bi+1Ci+1
+        n = grammar.productions[i].right.length
+        m = grammar.productions[i].left.length
 
-            //A->B1C1
-            //left side = grammar.productions[i].left
-            //right side = [grammar.productions[i].right[0], "C1"]
-
-
-            //C1->B2C2..Bn-3Cn-3
-            for(let j = 1; j < grammar.productions[i].right.length - 2; j++){
-                //left_side of production = ["C" + j.toString()]
-                //right side of production = [grammar.productions[i].right[j+1], "C" + (j + 1).toString()]
-            }
-
-            //Cn-2->Bn-1Bn
-            //n = grammar.productions[i].right.length
-            //left_side = ["C" + (n - 2).toString()]
-            //right side = [grammar.productions[i].right[n-1], grammar.productions[i].right[n]]
-        }
-    }
-
-    for(const i in grammar.productions){
-        if(i.left.length + 2 <=  i.right.length){
+        if(m + 2 <= n){
             //new Variables D2..Dn-1
 
             //A1A2->B1D2
-            //left side = [grammar.productions[i].left[0], grammar.productions[i].left[1]]
-            //right side = [grammar.productions[i].right[0], "D2"]
-
-
+            grammar.productions.push(new Production([grammar.productions[i].left[0], grammar.productions[i].left[1]], [grammar.productions[i].right[0], "D1"]))
+            
             //DiAi+1->BiDi+1
-            for(let j = 2; j < grammar.productions[i].right.length - 1; j++){
-                //left_side of production = ["D" + j.toString(), grammar.productions[i].left[j+1]]
-                //right side of production = [grammar.productions[i].right[j], "D" + (j + 1).toString()]
+            for(let j = 1; j < m - 1; j++){
+                grammar.productions.push(new Production(["D" + j.toString(), grammar.productions[i].left[j+1]], [grammar.productions[i].right[j], "D" + (j + 1).toString()]))
+            }
+
+            //Di->BiDi+1
+            for(let j = m - 1; j < n - 2; j++){
+                grammar.productions.push(new Production(["D" + j.toString()], [grammar.productions[i].right[j], "D" + (j + 1).toString()]))
             }
 
             //Dn-1->BnBn+1
-            //n = grammar.productions[i].right.length
-            //left_side = ["D" + (n - 1).toString()]
-            //right side = [grammar.productions[i].right[n], grammar.productions[i].right[n+1]]
+            grammar.productions.push(new Production(["D" + (n - 2).toString()], [grammar.productions[i].right[n-2], grammar.productions[i].right[n-1]]))
+
+            //remove replaced production
+            grammar.productions.splice(i, 1)
+            i -= 1 
         }
     }
 
-    for(const i in grammar.productions){
-        if(i.left.length  == i.right.length + 1 && i.left.length >= 2){
+    for(let i = 0; i < grammar.productions.length; i++){
+        n = grammar.productions[i].left.length
+
+        if(grammar.productions[i].right.length == n + 1 && n >= 2){
             //new Variables D2..Dn
 
             //A1A2->B1D2
-            //left side = [grammar.productions[i].left[0], grammar.productions[i].left[1]]
-            //right side = [grammar.productions[i].right[0], "D2"]
-
+            grammar.productions.push(new Production([grammar.productions[i].left[0], grammar.productions[i].left[1]], [grammar.productions[i].right[0], "D2"]))
 
             //DiAi+1->BiDi+1
-            for(let j = 2; j < grammar.productions[i].right.length - 1; j++){
-                //left_side of production = ["D" + j.toString(), grammar.productions[i].left[j+1]]
-                //right side of production = [grammar.productions[i].right[j], "D" + (j + 1).toString()]
+            for(let j = 1; j < n - 1; j++){
+                grammar.productions.push(new Production(["D" + j.toString(), grammar.productions[i].left[j+1]], [grammar.productions[i].right[j], "D" + (j + 1).toString()]))
             }
 
             //Dn-1->BnBn+1
-            //n = grammar.productions[i].right.length
-            //left_side = ["D" + (n - 1).toString()]
-            //right side = [grammar.productions[i].right[n], grammar.productions[i].right[n+1]]
+            grammar.productions.push(new Production(["D" + (n - 1).toString()], [grammar.productions[i].right[n-1], grammar.productions[i].right[n]]))
 
+            //remove replaced production
+            grammar.productions.splice(i, 1)
+            i -= 1
         }
     }
 
-    for(const i in grammar.productions){
-        if(i.left.length  == i.right.length && i.left.length > 2){
+    for(let i = 0; i < grammar.productions.length; i++){
+        n = grammar.productions[i].right.length
+        m = grammar.productions[i].left.length
+
+        if(m == n && n > 2){
              //new Variables D2..Dn-1
 
             //A1A2->B1D2
-            //left side = [grammar.productions[i].left[0], grammar.productions[i].left[1]]
-            //right side = [grammar.productions[i].right[0], "D2"]
-
+            grammar.productions.push(new Production([grammar.productions[i].left[0], grammar.productions[i].left[1]], [grammar.productions[i].right[0], "D2"]))
 
             //DiAi+1->BiDi+1
-            for(let j = 2; j < grammar.productions[i].right.length - 1; j++){
-                //left_side of production = ["D" + j.toString(), grammar.productions[i].left[j+1]]
-                //right side of production = [grammar.productions[i].right[j], "D" + (j + 1).toString()]
+            for(let j = 1; j < n - 2; j++){
+                grammar.productions.push(new Production(["D" + j.toString(), grammar.productions[i].left[j+1]], [grammar.productions[i].right[j], "D" + (j + 1).toString()]))
             }
 
             //Dn-1An->Bn-1Bn
-            //n = grammar.productions[i].right.length
-            //left_side = ["D" + (n - 1).toString(), grammar.productions[i].left[n]]
-            //right side = [grammar.productions[i].right[n-1], grammar.productions[i].right[n]]
+            n = grammar.productions[i].right.length
+            grammar.productions.push(new Production(["D" + (n - 2).toString(), grammar.productions[i].left[n-1]], [grammar.productions[i].right[n-2], grammar.productions[i].right[n-1]]))
+
+            //remove replaced production
+            grammar.productions.splice(i, 1)
+            i -= 1
         }
-    }   
+    }
+
+    return grammar
 }
 
 /**
@@ -153,4 +180,29 @@ function grammar_to_lba(grammar){
             throw new Error('not in kuroda normalform')
         }
     }
+}
+
+nonterminals = ['S', 'B']
+terminals = ['a', 'b', 'c']
+start = 'S'
+productions = [new Production(['S'], ['a', 'S', 'B', 'c']), 
+                new Production(['S'], ['a', 'b', 'c']),
+                new Production(['c', 'B'], ['B', 'c']),  
+                new Production(['b', 'B'], ['b', 'b']) 
+            ]
+productions2 = [new Production(['S', 'B'], ['a', 'S', 'B', 'c']), 
+        ]
+productions3 = [new Production(['S', 'B', 'a', 'b'], ['a', 'S', 'B', 'c']), 
+    ]
+
+console.log('old grammar:')
+for(let production of productions){
+    console.log(production.left + " -> " + production.right)
+}
+
+grammar = convert_to_kuroda(new Grammar(nonterminals, terminals, productions, start))
+
+console.log('\nnew grammar:')
+for(let production of grammar.productions){
+    console.log(production.left + " -> " + production.right)
 }
